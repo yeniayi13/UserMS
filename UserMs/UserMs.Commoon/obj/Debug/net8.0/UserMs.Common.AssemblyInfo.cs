29cc236f5b54e52170d73c7c4d72915914bf3bf3
yeniayi13 <115005952+yeniayi13@users.noreply.github.com>
2025-04-28 19:35:00 -0400

    gitignore

--- UserMs/UserMs.Commoon/obj/Debug/net8.0/UserMs.Common.AssemblyInfo.cs
+++ UserMs/UserMs.Commoon/obj/Debug/net8.0/UserMs.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserMs.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb46d221628b86ae32db5f118d1b449d84a8511")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a8c740c889ed463d826773e26ab3a73b42d33a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserMs.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserMs.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
